f6e16a2bc4dd99a9b68c5c5ac9aad3591b8f8e29
tutorgaming <tutorgaming@gmail.com>
2023-04-12 19:43:39 +0700

    Initial Commit

---
+++ dataloaders/isl2Places_dataset.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Pytorch Dataset Class for ISL2Places Dataset
+"""
+# Author : Theppasith N. <tutorgaming@gmail.com>
+# Date : 07-Apr-2023
+#####################################################################
+# Imports
+#####################################################################
+import os
+import glob
+from PIL import Image
+from torch.utils.data import Dataset
+
+#####################################################################
+# Class
+#####################################################################
+class ISL2PlacesDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = glob.glob(os.path.join(self.root_dir, '*.jpg'))
+        self.image_paths.sort()
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx])
+        image = image.convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image
