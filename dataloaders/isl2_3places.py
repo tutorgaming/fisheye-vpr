66c0218d4f43c8164eb9f70e7c93ba51495374ab
Tutorgaming <tutorgaming@gmail.com>
2023-06-09 17:03:06 +0700

    Snapshot from 09-Jun-2023

---
+++ dataloaders/isl2_3places.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+ISL2 3Places Toy Dataset and Dataloaders
+"""
+# Author : Theppasith N. <tutorgaming@gmail.com>
+# Date : 0-Jun-2023
+#####################################################################
+# Imports
+#####################################################################
