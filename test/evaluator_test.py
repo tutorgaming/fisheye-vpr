d017d2dbe33ea7b3f78c0c936292febcb5d35cb5
Theppasith N. (GPU1) <theppasith.n@obodroid.com>
2025-01-03 00:39:29 +0700

    Testing the Place Recognition Evaluator Module using example trained model loading

--- test/evaluator_test.py
+++ test/evaluator_test.py
@@ -14,7 +14,7 @@ from main import VPR
 class TestEvaluator(unittest.TestCase):
     def setUp(self):
         # Setup test data paths
-        self.result_path = Path("/workspace/results/20250101/resnet18_NetVLAD64_IsaacOfficeAll_Fisheye_01-Jan-2025_11-50-31")
+        self.result_path = Path("/workspace/old_results/20250102/resnet18finetuned_NetVLAD64_IsaacOfficeAll_Fisheye_02-Jan-2025_21-40-49")
         self.result_json = self.result_path / "result.json"
 
         # Load results and config
@@ -31,7 +31,7 @@ class TestEvaluator(unittest.TestCase):
 
         # Load best checkpoint
         checkpoint = torch.load(self.best_model_path)
-        self.vpr.model.load_state_dict(checkpoint["model_state_dict"])
+        self.vpr.model.load_state_dict(checkpoint)
 
         # Create evaluator
         self.evaluator = Evaluator(
@@ -56,19 +56,25 @@ class TestEvaluator(unittest.TestCase):
 
     def test_evaluation(self):
         # Run evaluation
-        recall_k = 1 # Test with recall@1
-        avg_recall, results = self.evaluator.evaluate(k=recall_k)
+        recalls = []
+        # recall_k = 10 # Test with recall@1
+        for k in range(1,11):
+            avg_recall, results = self.evaluator.evaluate(k=k)
+            recalls.append(avg_recall)
 
-        # Basic assertions
-        self.assertIsInstance(avg_recall, float)
-        self.assertGreaterEqual(avg_recall, 0.0)
-        self.assertLessEqual(avg_recall, 1.0)
+            # Basic assertions
+            self.assertIsInstance(avg_recall, float)
+            self.assertGreaterEqual(avg_recall, 0.0)
+            self.assertLessEqual(avg_recall, 1.0)
 
-        # Check results list
-        self.assertIsInstance(results, list)
-        self.assertGreater(len(results), 0)
+            # Check results list
+            self.assertIsInstance(results, list)
+            self.assertGreater(len(results), 0)
 
-        print(f"Average Recall@{recall_k}: {avg_recall}")
+            # print(f"Average Recall@{k}: {avg_recall}")
+
+        for k, recall_at_k in enumerate(recalls):
+            print(f"Recall@{k+1} = {recall_at_k}")
 
 if __name__ == '__main__':
     unittest.main()
