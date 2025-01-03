b154c5cf2b8dd862e1ca1551542975369bf5f98a
Theppasith N. (GPU1) <theppasith.n@obodroid.com>
2025-01-04 00:44:56 +0700

    hloc model loading test

--- test/matlab_model_load_test.py
+++ test/matlab_model_load_test.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append("/Users/tutorgaming/Repository/workspace/fisheye-vpr")
 
+# If exists
+sys.path.append("/workspace/fisheye-vpr")
+
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -9,11 +12,27 @@ from dataloaders.isl2_3places_fisheye import ISL2_3Places_Fisheye_Dataset
 from models.encoders.hloc_vgg16 import HLOCVGG16Encoder
 from models.clustering.hloc_netvlad import HLOCNetVLAD
 
-class ModelLoadTest(object):
+# TOrch Summary
+import torchinfo
+
+# class ModelLoadTest(object):
+#     def __init__(self):
+#         self.dataset = ISL2_3Places_Fisheye_Dataset()
+#         self.encoder = HLOCVGG16Encoder()
+#         self.clustering = HLOCNetVLAD()
+
+class HLOC_VPR(torch.nn.Module):
     def __init__(self):
-        self.dataset = ISL2_3Places_Fisheye_Dataset()
+        super(HLOC_VPR, self).__init__()
         self.encoder = HLOCVGG16Encoder()
         self.clustering = HLOCNetVLAD()
 
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.clustering(x)
+        return x
+
 if __name__ == "__main__":
-    ModelLoadTest()
+    dataset = ISL2_3Places_Fisheye_Dataset()
+    model = HLOC_VPR()
+    torchinfo.summary(model, input_size=(1,3,256,256))
